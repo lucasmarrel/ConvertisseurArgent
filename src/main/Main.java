c54ffcb0c2baf90b5f7ab1e7e79ec59fb24cfe62
lucasmarrel <lucas.marrel@sfr.fr>
2017-08-21 19:44:15 -0400

    premier commit

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import vue.Fenetre;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//System.out.println("Hey");
+		Fenetre fen = new Fenetre();
+		fen.setVisible(true);
+
+	}
+
+}
---
+++ src/vue/Fenetre.java
@@ -0,0 +1,30 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Fenetre extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public Fenetre() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 846, 462);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
