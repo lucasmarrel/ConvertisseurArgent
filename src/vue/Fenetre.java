47c62b09ce7deec40ffd03aa82ef9318203e0bc0
lucasmarrel <lucas.marrel@sfr.fr>
2017-08-22 21:33:02 -0400

    modif resultat conversion

--- src/vue/Fenetre.java
+++ src/vue/Fenetre.java
@@ -32,7 +32,7 @@ public class Fenetre extends JFrame {
 	 */
 	public Fenetre() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 846, 462);
+		setBounds(100, 100, 846, 224);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -80,31 +80,31 @@ public class Fenetre extends JFrame {
 					montant = Float.parseFloat(txtMontant.getText());
 				
 					if (cbDevise1.getSelectedIndex()==cbDevise2.getSelectedIndex()){
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 					else if (cbDevise1.getSelectedIndex()==0 && cbDevise2.getSelectedIndex()==1){
 						montant = conversion.convertisseurEurACad(montant);
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 					else if (cbDevise1.getSelectedIndex()==0 && cbDevise2.getSelectedIndex()==2){
 						montant = conversion.convertisseurEurAUsd(montant);
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 					else if (cbDevise1.getSelectedIndex()==1 && cbDevise2.getSelectedIndex()==0){
 						montant = conversion.convertisseurCadAEur(montant);
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 					else if (cbDevise1.getSelectedIndex()==1 && cbDevise2.getSelectedIndex()==2){
 						montant = conversion.convertisseurCadAUsd(montant);
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 					else if (cbDevise1.getSelectedIndex()==2 && cbDevise2.getSelectedIndex()==0){
 						montant = conversion.convertisseurUsdAEur(montant);
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 					else if (cbDevise1.getSelectedIndex()==2 && cbDevise2.getSelectedIndex()==1){
 						montant = conversion.convertisseurUsdACad(montant);
-						lbResultat.setText(String.valueOf(montant));
+						lbResultat.setText(String.valueOf(montant)+" "+cbDevise2.getSelectedItem());
 					}
 				} catch (Exception ex){
 					lbResultat.setText("Le montant entré est incorrect");
